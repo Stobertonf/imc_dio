a1095404a187353ad3746144c2d47fb9e9729f34
stobertonf <stobertonf@gmail.com>
2023-03-11 17:53:31 -0300

    styles(Bloc): Creating palette colors file.

---
+++ lib/app/shared/color_palette.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const Color textColor = Color(0xff161616);
+const Color darkRedColor = Color(0xff9D3514);
+const Color errorModalProdutcColor = Color(0xffc23939);
