d180411cd0e564d3eeb505f32d8e4de73077d5ab
stobertonf <stobertonf@gmail.com>
2023-03-11 16:26:09 -0300

    feat(imc): Creating front-end files.

---
+++ lib/ui/ios/cupertino_app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+import 'package:imc_dio/ui/ios/pages/home_page.dart';
+
+class MyCupertinoApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoApp(
+      title: "IMC",
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
