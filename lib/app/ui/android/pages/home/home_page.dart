9ac81ca280499c15ccb9cdf6cfda2e670601f29c
stobertonf <stobertonf@gmail.com>
2023-04-29 16:24:10 -0300

    feat(sqLite): Creating the Imc Data Base file.

--- lib/app/ui/android/pages/home/home_page.dart
+++ lib/app/ui/android/pages/home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sqflite/sqflite.dart';
 import 'package:imc_dio/app/shared/components/snackBar/message_snack_bar.dart';
 
 class HomePage extends StatefulWidget {
@@ -6,7 +7,7 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage>with  MessageSnackBar {
+class _HomePageState extends State<HomePage> with MessageSnackBar {
   late String _result;
   List<double> imcList = [];
   GlobalKey<FormState> _formKey = GlobalKey<FormState>();
@@ -23,12 +24,10 @@ class _HomePageState extends State<HomePage>with  MessageSnackBar {
     _weightController.text = '';
     _heightController.text = '';
     setState(() {
-      
       _result = 'Informe seus dados';
     });
-      imcList.clear();
+    imcList.clear();
   }
-
 
   void showErrorMessageSnackBar(String message) {
     final snackBar = SnackBar(
@@ -38,23 +37,22 @@ class _HomePageState extends State<HomePage>with  MessageSnackBar {
     ScaffoldMessenger.of(context).showSnackBar(snackBar);
   }
 
-
   void calculateImc() {
-  double weight = double.tryParse(_weightController.text) ?? 0.0;
-  double height = double.tryParse(_heightController.text) ?? 0.0;
-  if (weight == 0.0) {
-    showErrorMessageSnackBar("Insira um peso válido!");
-    return;
-  }
-  if (height == 0.0) {
-    showErrorMessageSnackBar("Insira uma altura válida!");
-    return;
-  }
-  height /= 100.0;
-  
-  double imc = weight / (height * height);
-  setState(() {
-    _result = "IMC = ${imc.toStringAsPrecision(2)}\n";
+    double weight = double.tryParse(_weightController.text) ?? 0.0;
+    double height = double.tryParse(_heightController.text) ?? 0.0;
+    if (weight == 0.0) {
+      showErrorMessageSnackBar("Insira um peso válido!");
+      return;
+    }
+    if (height == 0.0) {
+      showErrorMessageSnackBar("Insira uma altura válida!");
+      return;
+    }
+    height /= 100.0;
+
+    double imc = weight / (height * height);
+    setState(() {
+      _result = "IMC = ${imc.toStringAsPrecision(2)}\n";
       if (imc < 18.6)
         _result += "Abaixo do peso";
       else if (imc < 24.9)
@@ -67,37 +65,37 @@ class _HomePageState extends State<HomePage>with  MessageSnackBar {
         _result += "Obesidade Grau II";
       else
         _result += "Obesidade Grau III";
-    imcList.add(imc);
-  });
-}
+      imcList.add(imc);
+    });
+  }
 
-Widget buildIMCList() {
-  return SizedBox(
-    height: 200,
-    child: ListView.builder(
-      itemCount: imcList.length,
-      itemBuilder: (BuildContext context, int index) {
-        double imc = imcList[index];
-        String message = "IMC = ${imc.toStringAsPrecision(2)} ";
-        if (imc < 18.6)
-          _result += "Abaixo do peso";
-        else if (imc < 50.0)
-          _result += "Peso ideal";
-        else if (imc < 60.0)
-          _result += "Levemente acima do peso";
-        else if (imc < 75.0)
-          _result += "Obesidade Grau I";
-        else if (imc < 90.0)
-          _result += "Obesidade Grau II";
-        else
-          _result += "Obesidade Grau IIII";
-        return ListTile(
-          title: Text(message),
-        );
-      },
-    ),
-  );
-}
+  Widget buildIMCList() {
+    return SizedBox(
+      height: 200,
+      child: ListView.builder(
+        itemCount: imcList.length,
+        itemBuilder: (BuildContext context, int index) {
+          double imc = imcList[index];
+          String message = "IMC = ${imc.toStringAsPrecision(2)} ";
+          if (imc < 18.6)
+            _result += "Abaixo do peso";
+          else if (imc < 50.0)
+            _result += "Peso ideal";
+          else if (imc < 60.0)
+            _result += "Levemente acima do peso";
+          else if (imc < 75.0)
+            _result += "Obesidade Grau I";
+          else if (imc < 90.0)
+            _result += "Obesidade Grau II";
+          else
+            _result += "Obesidade Grau IIII";
+          return ListTile(
+            title: Text(message),
+          );
+        },
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -148,7 +146,7 @@ Widget buildIMCList() {
               controller: _heightController),
           buildTextResult(),
           buildCalculateButton(),
-           buildIMCList(),
+          buildIMCList(),
         ],
       ),
     );
